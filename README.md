7f34bded678d55181cfef1b5922f5f65147106e1
mariekatipunan <marie.katipunan@lsgh.edu.ph>
2021-04-29 21:32:06 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# autoloan-zikn9p
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/autoloan-zikn9p)
---
+++ pages/home/home.ts
@@ -0,0 +1,28 @@
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+
+@Component({
+  selector: 'page-home',
+  templateUrl: 'home.html',
+  styleUrls: ['home.css']
+})
+export class HomePage {
+loan: number;
+price : number;
+months: number;
+downpayment: number;
+loanamount: number;
+monthlypayment: number;
+downpaymentamount: number;
+totalinterest: number;
+
+constructor(public navCtrl: NavController) {}
+
+calculateLoan() {
+  this.downpaymentamount=this.price*this.downpayment/100
+  this.loanamount=this.price - this.downpaymentamount
+  this.totalinterest=this.loanamount * 0.3026
+  this.monthlypayment=(this.loanamount + this.totalinterest)/this.months
+  }
+}
+
